0273ad0864f9f743dca32fbe8045535660f15aa2
Zo <70714420+LorenzoBalderrama@users.noreply.github.com>
2023-05-29 20:56:22 -0600

    Updated HomeViewController
    Commit: I changed the letter style of capitalizing the letter of each word within the categories

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -73,12 +73,17 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-        guard let header = view as? UITableViewHeaderFooterView else {return}
+        guard let header = view as? UITableViewHeaderFooterView else { return }
         header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
         header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
         header.textLabel?.textColor = .white
-        header.textLabel?.text = header.textLabel?.text?.lowercased()
+        
+        if let text = header.textLabel?.text {
+            let capitalizedText = text.capitalized
+            header.textLabel?.text = capitalizedText
+        }
     }
+
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sectionTitles[section]
